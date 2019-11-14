98d1a38870389441ca66207e0d46bf19e918e750
josephabirached <josephabirached0@gmail.com>
2019-11-14 23:08:07 +0200

    Initial Commit

---
+++ OA.Services/ICalculatorServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OA.Data;
+
+namespace OA.Services
+{
+    public interface ICalculatorServices
+    {
+        CalculatorItems SumCalculatorItems(CalculatorItems items);
+
+        CalculatorItems MultiplyCalculatorItems(CalculatorItems items);
+
+        CalculatorItems DivideCalculatorItems(CalculatorItems items);
+
+        CalculatorItems PowerCalculatorItems(CalculatorItems items);
+
+        CalculatorItems SquareRootCalculatorItems(CalculatorItems items);
+    }
+}
